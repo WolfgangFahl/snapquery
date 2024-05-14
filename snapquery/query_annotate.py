136f24dc67f0e7c1b868fa23cb00822a5f7fc67b
tholzheim <tim.holzheim@rwth-aachen.de>
2024-05-15 01:03:46 +0200

    added title to annotated link

--- snapquery/query_annotate.py
+++ snapquery/query_annotate.py
@@ -3,17 +3,17 @@ Created on 2024-05-15
 
 @author: tholzheim
 """
+
 from bs4 import BeautifulSoup
 from lodstorage.query import Query, QuerySyntaxHighlight
 
 
 class SparqlQueryAnnotater:
-
     def __init__(self, query: Query):
         self.query = query
         query_syntax_highlight = QuerySyntaxHighlight(query)
         html = query_syntax_highlight.highlight()
-        self.soup = BeautifulSoup(html, 'html.parser')
+        self.soup = BeautifulSoup(html, "html.parser")
 
     def get_used_properties(self):
         prefix_element = self.soup.find_all("span", {"class": "nn"})
@@ -30,7 +30,9 @@ class SparqlQueryAnnotater:
             prefix = element
             colon = element.next_sibling
             item = element.next_sibling.next_sibling
-            annotation_element = self.soup.new_tag("a", href="http://www.wikidata.org/entity/" + item.text)
+            annotation_element = self.soup.new_tag(
+                "a", href="http://www.wikidata.org/entity/" + item.text, title=item.text
+            )
             prefix.insert_before(annotation_element)
             annotation_element.insert(0, prefix)
             annotation_element.insert(1, colon)

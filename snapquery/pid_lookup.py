cf96fa889fb8b697b50d10615a48fdd013d3d7f0
tholzheim <tim.holzheim@rwth-aachen.de>
2024-05-27 19:09:21 +0200

    formatted pid_lookup.py

--- snapquery/pid_lookup.py
+++ snapquery/pid_lookup.py
@@ -2,40 +2,42 @@
 Created on 2024-05-26
 @ author: wf
 """
+
 from ez_wikidata.wdsearch import WikidataSearch
 from snapquery.pid import PIDs, PIDValue, PID
 from snapquery.models.person import Person
 from typing import List
 from snapquery.snapquery_core import NamedQueryManager, NamedQuery
 
+
 class PersonLookup:
     """
     Lookup potential persons
     """
-    
-    def __init__(self,nqm:NamedQueryManager):
+
+    def __init__(self, nqm: NamedQueryManager):
         """
         constructor
         """
         self.pids = PIDs()
-        self.nqm=nqm
+        self.nqm = nqm
         self.wikidata_search = WikidataSearch()
 
-    def suggest(self, search_name: str, limit:int=10) -> List[Person]:
+    def suggest(self, search_name: str, limit: int = 10) -> List[Person]:
         """
         Suggest names using WikidataSearch
 
         Args:
             search_name (str): The name to search for suggestions.
         """
-        persons=[]
-        suggestions = self.wikidata_search.searchOptions(search_name,limit=limit)
-        qid_list=""
-        delim=""
-        for qid,_plabel,_pdesc in suggestions:
-            qid_list+=f"{delim}wd:{qid}"
-            delim=" "
-        named_query=NamedQuery(
+        persons = []
+        suggestions = self.wikidata_search.searchOptions(search_name, limit=limit)
+        qid_list = ""
+        delim = ""
+        for qid, _plabel, _pdesc in suggestions:
+            qid_list += f"{delim}wd:{qid}"
+            delim = " "
+        named_query = NamedQuery(
             namespace="pid-lookup",
             name="person-by-qid",
             title="Lookup persons with the given qids",
@@ -59,25 +61,25 @@ WHERE
   FILTER(lang(?given_name) = "en")
   FILTER(lang(?family_name) = "en")
 }
-            """
+            """,
         )
-        params_dict={"qid_list":qid_list}
-        person_lod,stats=self.nqm.execute_query(
+        params_dict = {"qid_list": qid_list}
+        person_lod, stats = self.nqm.execute_query(
             named_query=named_query,
             params_dict=params_dict,
             limit=limit,
-            with_stats=False)
+            with_stats=False,
+        )
         for pr in person_lod:
             person = Person(
                 label=pr.get("label"),
                 given_name=pr.get("given_name"),
                 family_name=pr.get("family_name"),
-                wikidata_id=pr.get("scholar").split('/')[-1],
+                wikidata_id=pr.get("scholar").split("/")[-1],
                 dblp_author_id=pr.get("dblp_author_id"),
                 orcid_id=pr.get("orcid_id"),
-                image=pr.get("image")
+                image=pr.get("image"),
             )
             persons.append(person)
-            
+
         return persons
-        

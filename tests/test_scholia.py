1bbb0c2b4a4bd50dc206342687428eb278184a6e
Wolfgang Fahl <wf@bitplan.com>
2024-06-28 09:44:08 +0200

    fixes scholia test

--- tests/test_scholia.py
+++ tests/test_scholia.py
@@ -33,7 +33,7 @@ class TestScholia(Basetest):
             limit = 10
         else:
             limit = None
-            # limit=10
+            #limit=10
 
         # Create a NamedQueryManager and ScholiaQueries instance
         nqm = NamedQueryManager.from_samples(db_path=db_path)
@@ -45,5 +45,5 @@ class TestScholia(Basetest):
         scholia_queries.save_to_json("/tmp/scholia.json")
 
         # Verify the data was stored
-        records = nqm.sql_db.query("SELECT * FROM NamedQuery WHERE namespace='scholia'")
+        records = nqm.sql_db.query(f"SELECT * FROM NamedQuery WHERE namespace='{scholia_queries.named_query_set.namespace}'")
         self.assertEqual(len(records), len(scholia_queries.named_query_set.queries))

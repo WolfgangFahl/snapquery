2a3602d2f294fa516a1589b20e841c9e20893d6f
tholzheim <tim.holzheim@rwth-aachen.de>
2024-07-03 15:12:21 +0200

    added SparqlAnalyzer

---
+++ snapquery/sparql_analyzer.py
@@ -0,0 +1,314 @@
+import logging
+import random
+import re
+from collections import Counter
+from typing import Iterable
+
+from rdflib.plugins.sparql import prepareQuery
+from rdflib.plugins.sparql.parser import parseQuery
+from rdflib.plugins.sparql.parserutils import CompValue
+from jinja2 import Template, Environment, meta
+
+
+logger = logging.getLogger(__name__)
+
+
+class SparqlAnalyzer:
+    """
+    SPARQL Query Analyzer
+    """
+
+    BLAZEGRAPH_NAMED_SUBQUERY_PATTERN = r"""WITH[\s\n]*(#[\w\s://\.\n,]+)?{(#[\w\s://\.\n,]+)?[\s\n](?P<subquery>[\n\r\b\w\d:\t\.";,\{\)\(\?\}\W#]*?)\s+[Aa][Ss]\s+%(?P<name>[A-Za-z\d_]+)"""
+    @classmethod
+    def get_prefix_luts(cls) -> dict[str, str]:
+        return {
+            "biopax": "http://www.biopax.org/release/biopax-level3.owl#",
+            "bd": "http://www.bigdata.com/rdf#",
+            "cc": "http://creativecommons.org/ns#",
+            "datacite": "http://purl.org/spar/datacite/",
+            "dblp": "https://dblp.org/rdf/schema#",
+            "dc": "http://purl.org/dc/elements/1.1/",
+            "dct": "http://purl.org/dc/terms/",
+            "freq": "http://purl.org/cld/freq/",
+            "geo": "http://www.opengis.net/ont/geosparql#",
+            "geof": "http://www.opengis.net/def/function/geosparql/",
+            "geom": "http://geovocab.org/geometry#",
+            "gpml": "http://vocabularies.wikipathways.org/gpml#",
+            "litre": "http://purl.org/spar/literal/",
+            "lgdo": "http://linkedgeodata.org/ontology/",
+            "ontolex": "http://www.w3.org/ns/lemon/ontolex#",
+            "orkgp": "http://orkg.org/orkg/predicate/",
+            "orkgc": "http://orkg.org/orkg/class/",
+            "orkgr": "http://orkg.org/orkg/resource/",
+            "owl": "http://www.w3.org/2002/07/owl#",
+            "p": "http://www.wikidata.org/prop/",
+            "pav": "http://purl.org/pav/",
+            "pq": "http://www.wikidata.org/prop/qualifier/",
+            "pqn": "http://www.wikidata.org/prop/qualifier/value-normalized/",
+            "pqv": "http://www.wikidata.org/prop/qualifier/value/",
+            "pr": "http://www.wikidata.org/prop/reference/",
+            "prn": "http://www.wikidata.org/prop/reference/value-normalized/",
+            "prov": "http://www.w3.org/ns/prov#",
+            "prv": "http://www.wikidata.org/prop/reference/value/",
+            "ps": "http://www.wikidata.org/prop/statement/",
+            "psn": "http://www.wikidata.org/prop/statement/value-normalized/",
+            "psv": "http://www.wikidata.org/prop/statement/value/",
+            "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+            "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+            "schema": "http://schema.org/",
+            "skos": "http://www.w3.org/2004/02/skos/core#",
+            "void": "http://rdfs.org/ns/void#",
+            "vrank": "http://purl.org/voc/vrank#",
+            "wd": "http://www.wikidata.org/entity/",
+            "wdata": "http://www.wikidata.org/wiki/Special:EntityData/",
+            "wdno": "http://www.wikidata.org/prop/novalue/",
+            "wdref": "http://www.wikidata.org/reference/",
+            "wds": "http://www.wikidata.org/entity/statement/",
+            "wdt": "http://www.wikidata.org/prop/direct/",
+            "wdtn": "http://www.wikidata.org/prop/direct-normalized/",
+            "wdv": "http://www.wikidata.org/value/",
+            "wikibase": "http://wikiba.se/ontology#",
+            "wp": "http://vocabularies.wikipathways.org/wp#",
+            "wprdf": "http://rdf.wikipathways.org/",
+            "xsd": "http://www.w3.org/2001/XMLSchema#",
+            "mwapi": "https://www.mediawiki.org/ontology#API/",
+            "hint": "http://www.bigdata.com/queryHints#",
+            "gas": "http://www.bigdata.com/rdf/gas#",
+        }
+
+    @classmethod
+    def prefix_clause(cls, prefix: str, iri: str) -> str:
+        """
+        Provide SPARQL refix clause for given prefix and url
+        Args:
+            prefix: prefix name
+            iri: iri
+
+        Returns:
+            prefix clause
+        """
+        return f"PREFIX {prefix}:  <{iri}>"
+
+    @classmethod
+    def extract_used_prefixes(cls, query: str) -> tuple[dict[str, str], set[str]]:
+        """
+        Extract used prefixes from SPARQL query
+        Args:
+            query: SPARQL query
+
+        Returns:
+            dict of declared prefixes
+        """
+        # add prefixes to avoid parsing error due to missing prefix
+        prefix_lut = cls.get_prefix_luts()
+        prefixed_query = cls._add_prefixes(prefix_lut, query)
+        parsed_query = parseQuery(prefixed_query)
+        elements = parsed_query.as_list()
+        defined_prefixes = []
+        used_prefixes = []
+        for element in elements:
+            if isinstance(element, CompValue) and element.name == "PrefixDecl":
+                defined_prefixes.append(element)
+            elif isinstance(element, CompValue) and element.name == "pname":
+                used_prefixes.append(element)
+            elif isinstance(element, Iterable) and not isinstance(element, str):
+                if isinstance(element, dict):
+                    elements.extend(element.values())
+                else:
+                    elements.extend(element)
+            else:
+                pass
+        declared_prefix_counter = Counter(
+            [value.get("prefix") for value in defined_prefixes]
+        )
+        multi_declarations = [
+            prefix for prefix, count in declared_prefix_counter.items() if count > 1
+        ]
+        used_prefix_names = {value.get("prefix") for value in used_prefixes}
+        used_prefix_map = dict()
+        for prefix_value in reversed(defined_prefixes):
+            prefix_name = prefix_value.get("prefix")
+            prefix_iri = prefix_value.get("iri")
+            if prefix_name in multi_declarations or prefix_name not in prefix_lut:
+                used_prefix_map[prefix_name] = str(prefix_iri)
+        return used_prefix_map, used_prefix_names
+
+    @classmethod
+    def add_missing_prefixes(cls, query: str):
+        """
+        Add missing prefixes to SPARQL query
+        Args:
+            query: SPARQL query
+
+        Returns:
+            SPARQL query
+        """
+        try:
+            # normalize query for parsing
+            prepared_query = query
+            if cls.has_parameter(prepared_query):
+                prepared_query = cls.fill_with_sample_query_parameters(prepared_query)
+            if cls.has_blazegraph_with_clause(prepared_query):
+                prepared_query = cls.transform_with_clause_to_subquery(prepared_query)
+            # extract used and declared prefixes
+            declared_prefixes, used_prefixes = cls.extract_used_prefixes(prepared_query)
+            missing_prefix_declarations = used_prefixes - set(declared_prefixes.keys())
+            undefined_prefixes = missing_prefix_declarations.difference(
+                cls.get_prefix_luts().keys()
+            )
+            if undefined_prefixes:
+                logger.error(
+                    f"Prefix definitions missing for: {undefined_prefixes} → Not all prefixes that are missing can be added"
+                )
+            missing_prefix_declarations_lut = {
+                key: value
+                for key, value in cls.get_prefix_luts().items()
+                if key in missing_prefix_declarations
+            }
+            fixed_query = cls._add_prefixes(missing_prefix_declarations_lut, query)
+        except Exception as e:
+            logger.debug(
+                "Adding missing prefixes to query failed → Unable to parse SPARQL query"
+            )
+            logging.error(e)
+            fixed_query = query
+        return fixed_query
+
+    @classmethod
+    def transform_with_clause_to_subquery(cls, query: str) -> str:
+        """
+        Transform blazegraph with clause to subquery statement
+        Args:
+            query:
+
+        Returns:
+
+        """
+        match = re.search(cls.BLAZEGRAPH_NAMED_SUBQUERY_PATTERN, query)
+        if match:
+            subquery = match.group("subquery")
+            name = match.group("name")
+            start_pos, end_pos = match.span()
+            # check if Where mus be added
+            select_part = query[:start_pos]
+            where_part = query[end_pos + 1:]
+            if cls.has_blazegraph_with_clause(where_part):
+                where_part = cls.transform_with_clause_to_subquery(where_part)
+            if where_part.lower().strip().startswith("where"):
+                query_with_removed = select_part + where_part
+            else:
+                query_with_removed = f"{select_part}\nWHERE\n{where_part}"
+
+            include_pattern = f"[Ii][Nn][Cc][Ll][Uu][Dd][Ee]\s+%{name}"
+            subquery = f"{{{subquery}\n"
+            query_transformed = re.sub(include_pattern, subquery, query_with_removed)
+            return query_transformed
+
+    @classmethod
+    def has_blazegraph_with_clause(cls, query: str) -> bool:
+        """
+        Check if the given query has a WITH clause (named subquery)
+        For details see https://github.com/blazegraph/database/wiki/NamedSubquery
+        Args:
+            query: SPARQL query
+
+        Returns:
+            True if the query has a WITH clause (named subquery)
+        """
+        match = re.search(cls.BLAZEGRAPH_NAMED_SUBQUERY_PATTERN, query)
+        return True if match else False
+
+    @classmethod
+    def _add_prefixes(cls, prefixes: dict[str, str], query: str) -> str:
+        """
+        Add prefixes to SPARQL query
+        Args:
+            prefixes: prefixes to add
+            query: SPARQL query
+
+        Returns:
+            SPARQL query with prefixes added
+        """
+        prefixes_clauses = [
+            cls.prefix_clause(prefix, iri) for prefix, iri in prefixes.items()
+        ]
+        prefixes_clauses_str = "\n".join(prefixes_clauses)
+        return prefixes_clauses_str + "\n" + query
+
+    @classmethod
+    def has_parameter(cls, query: str) -> bool:
+        """
+        Check if the given query has parameters that need to need set
+        Args:
+            query: SPARQL query
+
+        Returns:
+            True if the query has parameters that need to need set
+        """
+        vars = cls.get_query_parameter(query)
+        return len(vars) > 0
+
+    @classmethod
+    def get_query_parameter(cls, query: str) -> set[str]:
+        env = Environment()
+        ast = env.parse(query)
+        vars = meta.find_undeclared_variables(ast)
+        return vars
+
+    @classmethod
+    def fill_with_sample_query_parameters(cls, query: str) -> str:
+        """
+        Fill the given SPARQL query with sample query parameters
+        Args:
+            query: SPARQL query
+
+        Returns:
+
+        """
+        if not cls.has_parameter(query):
+            return query
+        parameter_names = cls.get_query_parameter(query)
+        params = cls._prepare_sample_parameter(parameter_names)
+        return cls.bind_parameters_to_query(query, params)
+
+    @classmethod
+    def bind_parameters_to_query(cls, query: str, params: dict[str, str]) -> str:
+        """
+        Bind the parameters to the given query
+        Args:
+            query: SPARQL query
+            params: quera params
+
+        Returns:
+            Query with parameters binded
+        """
+        template = Template(query)
+        query_with_param_values = template.render(**params)
+        return query_with_param_values
+
+    @classmethod
+    def _prepare_sample_parameter(cls, parameter_names: set[str]) -> dict[str, str]:
+        """
+        Prepare sample query parameters
+        """
+        params = dict()
+        for name in parameter_names:
+            params[name] = f"Q{random.randint(1, 1000)}"
+        return params
+
+    @classmethod
+    def is_valid(cls, query: str):
+        """
+        Check if query is valid SPARQL query
+        Args:
+            query: SPARQL query
+
+        Returns:
+            True if query is valid SPARQL query
+        """
+        try:
+            prepareQuery(query)
+            return True
+        except Exception as e:
+            logger.debug(f"Query is not valid SPARQL query: {e}")
+            return False
---
+++ tests/test_sparql_analyzer.py
@@ -0,0 +1,323 @@
+import logging
+import sys
+import unittest
+
+import snapquery.sparql_analyzer
+from snapquery.snapquery_core import NamedQueryManager
+from snapquery.sparql_analyzer import SparqlAnalyzer
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.root
+logger.addHandler(logging.StreamHandler(stream=sys.stdout))
+logger.setLevel(logging.DEBUG)
+
+
+class TestSparqlAnalyzer(unittest.TestCase):
+    """
+    Tests the Sparql Analyzer class.
+    """
+
+    def test_is_valid(self):
+        test_params = [
+            ("""SELECT DISTINCT * WHERE { ?astronaut wdt:P106 wd:Q11631 .}""", False),
+            (
+                """PREFIX wd: <http://www.wikidata.org/entity/>\nPREFIX wdt: <http://www.wikidata.org/prop/direct/>\n SELECT DISTINCT * WHERE { ?astronaut wdt:P106 wd:Q11631 .}""",
+                True,
+            ),
+        ]
+        for query, expected_res in test_params:
+            actual = SparqlAnalyzer.is_valid(query)
+            self.assertEqual(expected_res, actual)
+
+    @unittest.skip("Skip until the named query refactoring is finished")
+    def test_all_sample_queries(self):
+        """
+        test all sample queries.
+        """
+        nqm = NamedQueryManager()
+        namespaces = [
+            "challenge",
+            # "named_queries"  # all queries works except one which expects a datetime parameter which is not detected
+        ]
+        for namespace in namespaces:
+            queries = nqm.get_all_queries(namespace)
+            for query in queries:
+                with self.subTest(query=query):
+                    try:
+                        fixed_query = SparqlAnalyzer.add_missing_prefixes(query.sparql)
+                        if SparqlAnalyzer.has_parameter(fixed_query):
+                            fixed_query = SparqlAnalyzer.fill_with_sample_query_parameters(
+                                fixed_query
+                            )
+                        if SparqlAnalyzer.has_blazegraph_with_clause(fixed_query):
+                            fixed_query = SparqlAnalyzer.transform_with_clause_to_subquery(
+                                fixed_query
+                            )
+                        is_valid = SparqlAnalyzer.is_valid(fixed_query)
+                        if not is_valid:
+                            print(query.query_id)
+                        # self.assertTrue(is_valid)
+                    except Exception as e:
+                        print(query.query_id)
+                        raise e
+
+    def test_extract_used_prefixes(self):
+        """
+        test extract used prefixes.
+        """
+        sparql = """
+PREFIX imdb: <https://www.imdb.com/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+SELECT ?movie ?imdb_id ?title (MIN(YEAR(?date)) AS ?year) ?imdb_votes ?imdb_rating WHERE {
+  ?movie wdt:P31 wd:Q11424 .
+  ?movie wdt:P345 ?imdb_id .
+  ?movie wdt:P577 ?date .
+  ?movie rdfs:label ?title FILTER (LANG(?title) = "en") .
+  SERVICE <https://qlever.cs.uni-freiburg.de/api/imdb> {
+    ?movie_imdb imdb:id ?imdb_id .
+    ?movie_imdb imdb:type "movie" .
+    ?movie_imdb imdb:numVotes ?imdb_votes .
+    ?movie_imdb imdb:averageRating ?imdb_rating .
+  }
+}
+GROUP BY ?movie ?title ?imdb_id ?imdb_votes ?imdb_rating
+ORDER BY DESC(?imdb_votes)
+        """
+        declared_prefixes, used_prefixes = SparqlAnalyzer.extract_used_prefixes(sparql)
+        self.assertSetEqual({"rdfs", "imdb"}, set(declared_prefixes.keys()))
+        self.assertSetEqual({"wdt", "wd", "rdfs", "imdb"}, used_prefixes)
+
+    def test_add_missing_prefixes(self):
+        """
+        test add_missing_prefixes
+        """
+        sparql = """PREFIX imdb: <https://www.imdb.com/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+SELECT ?movie ?imdb_id ?title (MIN(YEAR(?date)) AS ?year) ?imdb_votes ?imdb_rating WHERE {
+  ?movie wdt:P31 wd:Q11424 .
+  ?movie wdt:P345 ?imdb_id .
+  ?movie wdt:P577 ?date .
+  ?movie rdfs:label ?title FILTER (LANG(?title) = "en") .
+  SERVICE <https://qlever.cs.uni-freiburg.de/api/imdb> {
+    ?movie_imdb imdb:id ?imdb_id .
+    ?movie_imdb imdb:type "movie" .
+    ?movie_imdb imdb:numVotes ?imdb_votes .
+    ?movie_imdb imdb:averageRating ?imdb_rating .
+  }
+}
+GROUP BY ?movie ?title ?imdb_id ?imdb_votes ?imdb_rating
+ORDER BY DESC(?imdb_votes)
+        """
+        expected_sparql = """PREFIX wd:  <http://www.wikidata.org/entity/>
+PREFIX wdt:  <http://www.wikidata.org/prop/direct/>
+PREFIX imdb: <https://www.imdb.com/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+SELECT ?movie ?imdb_id ?title (MIN(YEAR(?date)) AS ?year) ?imdb_votes ?imdb_rating WHERE {
+  ?movie wdt:P31 wd:Q11424 .
+  ?movie wdt:P345 ?imdb_id .
+  ?movie wdt:P577 ?date .
+  ?movie rdfs:label ?title FILTER (LANG(?title) = "en") .
+  SERVICE <https://qlever.cs.uni-freiburg.de/api/imdb> {
+    ?movie_imdb imdb:id ?imdb_id .
+    ?movie_imdb imdb:type "movie" .
+    ?movie_imdb imdb:numVotes ?imdb_votes .
+    ?movie_imdb imdb:averageRating ?imdb_rating .
+  }
+}
+GROUP BY ?movie ?title ?imdb_id ?imdb_votes ?imdb_rating
+ORDER BY DESC(?imdb_votes)
+        """
+        fixed_query = SparqlAnalyzer.add_missing_prefixes(sparql)
+        self.assertEqual(expected_sparql, fixed_query)
+
+    def test_transform_with_clause_to_subquery(self):
+        """
+        test transform_with_clause_to_subquery
+        """
+        query = """
+SELECT ?mol ?molLabel ?InChIKey ?CAS ?ChemSpider ?PubChem_CID WITH {
+  SELECT ?mol ?InChIKey WHERE {
+    SERVICE wikibase:mwapi {
+        bd:serviceParam wikibase:endpoint "www.wikidata.org";
+        wikibase:api "Search";
+        mwapi:srsearch "_shortkey_ haswbstatement:P235";
+        mwapi:srlimit "max".
+        ?mol wikibase:apiOutputItem mwapi:title.
+      }
+    ?mol wdt:P235 ?InChIKey .
+    FILTER (regex(str(?InChIKey), "^_shortkey_"))
+  }
+} AS %MOLS {
+  INCLUDE %MOLS
+  OPTIONAL { ?mol wdt:P231 ?CAS }
+  OPTIONAL { ?mol wdt:P661 ?ChemSpider }
+  OPTIONAL { ?mol wdt:P662 ?PubChem_CID }
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+}
+        """
+        expected_query = """
+SELECT ?mol ?molLabel ?InChIKey ?CAS ?ChemSpider ?PubChem_CID 
+WHERE
+{
+  {  SELECT ?mol ?InChIKey WHERE {
+    SERVICE wikibase:mwapi {
+        bd:serviceParam wikibase:endpoint "www.wikidata.org";
+        wikibase:api "Search";
+        mwapi:srsearch "_shortkey_ haswbstatement:P235";
+        mwapi:srlimit "max".
+        ?mol wikibase:apiOutputItem mwapi:title.
+      }
+    ?mol wdt:P235 ?InChIKey .
+    FILTER (regex(str(?InChIKey), "^_shortkey_"))
+  }
+}
+
+  OPTIONAL { ?mol wdt:P231 ?CAS }
+  OPTIONAL { ?mol wdt:P661 ?ChemSpider }
+  OPTIONAL { ?mol wdt:P662 ?PubChem_CID }
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+}
+        """
+
+        self.assertTrue(SparqlAnalyzer.has_blazegraph_with_clause(query))
+        self.assertFalse(SparqlAnalyzer.has_blazegraph_with_clause(expected_query))
+        fixed_query = SparqlAnalyzer.transform_with_clause_to_subquery(query)
+        self.assertEqual(expected_query, fixed_query)
+        self.assertFalse(SparqlAnalyzer.is_valid(query))
+        self.assertTrue(fixed_query)
+
+    def test_transform_with_clause_to_subquery_simple_example(self):
+        """
+        test transform_with_clause_to_subquery with a simple example
+        """
+        query = """
+SELECT ?country ?label WITH {
+  SELECT ?country WHERE {
+    wd:Q27230297 wdt:P495 ?country
+  }
+} AS %MOLS {
+  INCLUDE %MOLS
+  OPTIONAL { ?country rdfs:label ?label. FILTER(LANG(?label)="en") }
+}
+        """
+        self.assertTrue(SparqlAnalyzer.has_blazegraph_with_clause(query))
+        fixed_query = SparqlAnalyzer.transform_with_clause_to_subquery(query)
+        print(fixed_query)
+        self.assertFalse(SparqlAnalyzer.is_valid(query))
+        self.assertTrue(fixed_query)
+
+    def test_handling_of_parametrized_query(self):
+        """
+        tests handling of parametrized query
+        """
+        sparql = """
+ASK {
+    [] pq:P3712 / wdt:P31 wd:Q96471816 ; ps:P2860 / wdt:P1433 wd:{{q}} . BIND("venue-cito" AS ?aspectsubpage)
+}
+        """
+        has_parameter = SparqlAnalyzer.has_parameter(sparql)
+        self.assertTrue(has_parameter)
+        expected_params = {"q"}
+        query_params = SparqlAnalyzer.get_query_parameter(sparql)
+        self.assertSetEqual(expected_params, query_params)
+        filled_query = SparqlAnalyzer.fill_with_sample_query_parameters(sparql)
+        prefixed_filled_query = SparqlAnalyzer.add_missing_prefixes(filled_query)
+        self.assertTrue(SparqlAnalyzer.is_valid(prefixed_filled_query))
+
+    def test_transform_with_clause_to_subquery_with_multiple_clauses(self):
+        query = """
+#title: Variants of author name strings
+PREFIX target: <http://www.wikidata.org/entity/{{ q }}>
+
+SELECT 
+  (COUNT(?work) AS ?count)
+  ?string
+  (CONCAT("https://author-disambiguator.toolforge.org/names_oauth.php?doit=Look+for+author&name=", 
+        ENCODE_FOR_URI(?string)) AS ?author_resolver_url) 
+WITH
+{
+  # Find strings associated with the target author
+  SELECT DISTINCT ?string_
+  WHERE
+  {
+    { target: rdfs:label ?string_. } # in label
+    UNION
+    { target: skos:altLabel ?string_. } # in alias
+    UNION
+    {
+      ?author_statement ps:P50 target: ; 
+                        pq:P1932 ?string_. # in "stated as" strings for "author" statements on work items
+    }
+  }
+} AS %RAWstrings
+WITH
+# This part is due to Dipsacus fullonum, as per https://w.wiki/5Brk
+{
+  # Calculate capitalization variants of these raw strings
+  SELECT DISTINCT ?string
+  WHERE
+  {
+    {
+      INCLUDE %RAWstrings
+      BIND(STR(?string_) AS ?string) # the raw strings
+    }
+    UNION
+    {
+      INCLUDE %RAWstrings
+      BIND(UCASE(STR(?string_)) AS ?string) # uppercased versions of the raw strings
+    }
+    UNION
+    {
+      INCLUDE %RAWstrings
+      BIND(LCASE(STR(?string_)) AS ?string) # lowercased versions of the raw strings
+    }
+  }
+} AS %NORMALIZEDstrings
+WHERE {
+  # Find works that have "author name string" values equal to these normalized strings
+  INCLUDE %NORMALIZEDstrings
+  OPTIONAL { ?work wdt:P2093 ?string. }
+}
+GROUP BY ?string
+ORDER BY DESC (?count)
+
+        """
+        fixed_query = SparqlAnalyzer.add_missing_prefixes(query)
+        if SparqlAnalyzer.has_parameter(fixed_query):
+            fixed_query = SparqlAnalyzer.fill_with_sample_query_parameters(fixed_query)
+        if SparqlAnalyzer.has_blazegraph_with_clause(fixed_query):
+            fixed_query = SparqlAnalyzer.transform_with_clause_to_subquery(fixed_query)
+        is_valid = SparqlAnalyzer.is_valid(fixed_query)
+        self.assertTrue(is_valid)
+
+
+    def test_detect_undefined_prefixes(self):
+        """
+        Test detection for undefined prefixes.
+        """
+        query = "SELECT * WHERE {?work undefined:P2093 ?work . }"
+        with self.assertLogs(snapquery.sparql_analyzer.logger, level='INFO') as cm:
+            SparqlAnalyzer.add_missing_prefixes(query)
+            self.assertIn("ERROR:snapquery.sparql_analyzer:Prefix definitions missing for: {'undefined'} → Not all prefixes that are missing can be added", cm.output)
+
+    def test_add_missing_prefixes_on_incorreect_query(self):
+        """
+        Test add_missing_prefixes on query with syntax error making parsing impossible
+        """
+        query = "SELET * WHERE {??work undefined:P2093 ?work . }"
+        with self.assertLogs(snapquery.sparql_analyzer.logger, level=logging.DEBUG) as cm:
+            SparqlAnalyzer.add_missing_prefixes(query)
+            self.assertIn(
+                "DEBUG:snapquery.sparql_analyzer:Adding missing prefixes to query failed → Unable to parse SPARQL query",
+                cm.output)
+
+    def test_fill_with_sample_query_parameters_with_parameterless_query(self):
+        """
+        Test fill_with_sample_query_parameters with parameterless query.
+        """
+        query = "SELECT * WHERE {?work undefined:P2093 ?work . }"
+        filled_query = SparqlAnalyzer.fill_with_sample_query_parameters(query)
+        self.assertEqual(filled_query, query)
+
+if __name__ == "__main__":
+    unittest.main()
